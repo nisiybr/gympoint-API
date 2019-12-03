af2e3d01b1b16430e885c8862a1904da54db8ac8
nisiybr <guilherme.nisiyama@gmail.com>
2019-12-03 01:17:21 -0300

    Atualização

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import './database/index';
 import routes from './routes';
@@ -11,6 +12,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
   }
 
